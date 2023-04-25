ddf7c8ed01324a2fecb7cb9cabf371ad9b9acdb8
Kukulkaan <os.sid@yandex.ru>
2023-04-25 11:11:29 +0300

    minor fixes

--- stop.sh
+++ stop.sh
@@ -9,11 +9,12 @@ rm -rf ./temp/SPRO
 rm -rf ./temp/ZRDN1
 rm -rf ./temp/ZRDN2
 rm -rf ./temp/ZRDN3
+rm -rf ./temp/notalive
 
 rm -rf ./messages/TargetsLog
 rm -rf ./messages/WarningsLog
 rm -rf ./messages/StatusLog
 
 log="./logs/KPLOG.log"
-log2="./logs/KPFinal2.log"
+log2="./logs/KPFinal.log"
 cat $log | sort > $log2
